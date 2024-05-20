f915a8314e1d846198b4a64eb93c29f54b5faacf
David Varela <rd.varela@outlook.com>
2024-05-20 14:51:35 -0300

    primer commit

---
+++ script/script.js
@@ -0,0 +1,15 @@
+let nombre = document.getElementsByName("nombre")[0].value;
+let edad = document.getElementsByName("edad")[0].value;
+
+function formularioEnviar(){
+    event.preventDefault();
+    nombre = document.getElementsByName("nombre")[0].value;
+    edad = document.getElementsByName("edad")[0].value;
+
+    document.getElementById("edadUs").innerHTML = nombre;
+    document.getElementById("nombreUs").innerHTML = edad;
+
+    document.getElementById("resumen").style.display = "block";
+}
+
+console.log(nombre);
